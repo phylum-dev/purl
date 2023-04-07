5504a6245997b7809ef261046117c4996b5498c4
Matt Donoughe <matt.donoughe@phylum.io>
2023-04-07 10:08:30 -0400

    add platform qualifier for gem (#2)

---
+++ purl/src/qualifiers/well_known/gem.rs
@@ -0,0 +1,7 @@
+//! Known qualifier types for [gem].
+//!
+//! [gem]: https://github.com/package-url/purl-spec/blob/master/PURL-TYPES.rst#gem
+
+use super::str_ref_qualifier;
+
+str_ref_qualifier!(Platform, "platform", "platform");
